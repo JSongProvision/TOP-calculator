744a8476e6923f3986f447357885d97839077b7f
JSongProvision <142438994+JSongProvision@users.noreply.github.com>
2025-12-18 23:11:23 -0500

    Add operator function and html layout
    Creates the operator function used to determine correct math function
    based on input and creates some divs with buttons to arrange in a grid
    for calculator

--- main.js
+++ main.js
@@ -15,5 +15,24 @@ function multiply(num1, num2) {
 };
 
 function divide(numerator, denominator) {
-     return num1 / num2;
+     return numerator / denominator;
 };
+
+function operate(operator, num1, num2) {
+     switch (operator) {
+          case "add": 
+               return add(num1,num2);
+           case "subtract": 
+               return subtract(num1,num2);
+           case "multiply": 
+               return multiply(num1,num2);
+           case "divide": 
+               if (num2 !== 0) {
+                    return divide(num1,num2);  
+               } else
+                    return "You can't divide by 0 you fucking moron";
+               
+          default:
+               return "Some terrible bullshit has happened here, but I'm not sure what.";
+     }
+}
